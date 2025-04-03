c59fcc2dbea03901f79cb95329c3452ac4201a93
N.Sri Sabarish <140184657+Sri-Sabarish@users.noreply.github.com>
2025-04-03 08:50:50 +0530

    Update concatenating tuple.py

--- concatenating tuple.py
+++ concatenating tuple.py
@@ -1,7 +1,4 @@
-Python 3.6.0rc1 (v3.6.0rc1:29a273eee9a5, Dec  7 2016, 05:07:52) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:/Users/Naga-Meena-Sabari/AppData/Local/Programs/Python/Python36/concatenating tuple.py 
-(1, 2, 3, 4, 2, 3, 4, 5, 6, 7, 8, 9)
->>> 
-s
+a=(1,2,3)
+b=(4,5,6)
+c=a+b
+print(c)
